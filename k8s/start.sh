ffe3fdc6eb2f7645f58a54fe355f3c03e82ed54e
Abhishek-Punhani <punhani.manavabhi@gmail.com>
2025-10-31 01:30:14 +0530

    added monitoring services in k8s cluster
    Signed-off-by: Abhishek-Punhani <punhani.manavabhi@gmail.com>

--- k8s/start.sh
+++ k8s/start.sh
@@ -1,6 +1,10 @@
+# If getting error try to stop running services on these ports: 80, 4000, 6379, 9090, 3000, 3100
+
+# Using kind to create a local kubernetes cluster
 kind delete cluster --name kube-demo
 kind create cluster --image kindest/node:v1.29.4 --name kube-demo --config kind.yaml
 kubectl apply -f config.yaml
 kubectl apply -f deployment.yaml
-kubectl apply -f svc.yaml
+kubectl apply -f clusterIp.yaml
+kubectl apply -f nodePort.yaml
 kubectl apply -f load-balancer.yaml
