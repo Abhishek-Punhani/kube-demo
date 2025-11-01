9f3d4bc2001259100ef943e9cb92e4a3a22f89a1
Abhishek-Punhani <punhani.manavabhi@gmail.com>
2025-11-01 16:27:52 +0530

    adding argoCd
    Signed-off-by: Abhishek-Punhani <punhani.manavabhi@gmail.com>

--- k8s/start.sh
+++ k8s/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # If getting error try to stop running services on these ports: 80, 443
-
+# sudo systemctl stop nginx redis mysql
 # Using kind to create a local kubernetes cluster
 kind delete cluster --name kube-demo
 kind create cluster --image kindest/node:v1.29.4 --name kube-demo --config kind.yaml
@@ -17,22 +17,46 @@ kubectl wait --namespace ingress-nginx \
   --selector=app.kubernetes.io/component=controller \
   --timeout=90s
 
+# Add ArgoCd
+echo "Installing ArgoCD..."
+kubectl create namespace argocd || true
+helm repo add argo https://argoproj.github.io/argo-helm
+helm repo update
+helm install argocd argo/argo-cd \
+  --namespace argocd \
+  --version 7.6.12 \
+  --set server.service.type=ClusterIP \
+  --wait
+# Wait for ArgoCD server to be ready
+echo "Waiting for ArgoCD to be ready..."
+kubectl wait --namespace argocd \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/name=argocd-server \
+  --timeout=120s
+# Port-forward ArgoCD server
+# kubectl port-forward svc/argocd-server -n argocd 8080:443 
+echo "ArgoCD is available at http://localhost:8080 (username: admin, password: argocd-server initial password can be found with: kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath=\"{.data.password}\" | base64 -d; )"
+kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d
+echo ""
+
 # Apply application configs
 echo "Applying application configurations..."
 kubectl apply -f config.yaml
 kubectl apply -f deployment.yaml
 kubectl apply -f clusterIp.yaml
 kubectl apply -f ingress.yaml
-kubectl apply -f metrics-server.yaml
+kubectl apply -f argoCd.yaml
+# kubectl apply -f metrics-server.yaml
 
 echo "Deployment complete!"
 echo ""
 echo "Access your services at:"
-echo "  Frontend:     http://localhost"
-echo "  Demo Server:  http://localhost/api"
-echo "  Grafana:      http://localhost/grafana"
-echo "  Prometheus:   http://localhost/prometheus"
-echo "  Loki:         http://localhost/loki"
+echo " frontend :  http://localhost/"
+echo " demo server :  http://api.localhost"
+echo "  grafana :     http://grafana.localhost"
+echo "  prometheus :  http://prometheus.localhost"
+echo "  loki :        http://loki.localhost"
 echo ""
+
 echo "Wait a few moments for all pods to be ready, then check with:"
 echo "  kubectl get pods -A"
